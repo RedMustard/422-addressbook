9f44cc65e42e904d4622c9528c9ab5a31c0763b2
BrandonCao <brandonnhatcao@gmail.com>
2016-01-18 14:20:36 -0800

    add contact pop up works

--- AddressBook.py
+++ AddressBook.py
@@ -22,7 +22,7 @@ class mainWindow(object):
 
     def popup(self):
         self.w=AddContactWindow(self.master)
-        self.master.wait_window(self.w)
+        self.master.wait_window(self.w.top)
 
     def __init__(self,master):
         self.master = master
@@ -37,70 +37,71 @@ class mainWindow(object):
         self.book_list = Tk.Listbox(master, yscrollcommand = self.scrollbar.set)
         self.book_list.grid(row = 4, column = 1, padx = 10, pady = 10, columnspan= 3)
         self.scrollbar.config(command = self.book_list.yview)
-        self.add_button = Tk.Entry(master, text = 'Add', command = self.popup)
+        self.add_button = Tk.Button(master, text = 'Add', command = self.popup)
         self.add_button.grid(row= 5, column = 2)
         #ADD THE ADD BUTTON
 
 
-class AddContactWindow():
+class AddContactWindow(object):
 
-    def __init__():
+    def __init__(self,master):
+        top=self.top=Tk.Toplevel(master)
         self.master = master
         master.title('Add Contact')
 
-        self.first_name_label = Tk.Label(master, text = 'First Name:')
+        self.first_name_label = Tk.Label(top, text = 'First Name:')
         self.first_name_label.grid()
 
         #input for contacts first name
-        self.first_name = Tk.Entry(master)
+        self.first_name = Tk.Entry(top)
         self.first_name.grid(row = 0, column = 1)
 
-        self.last_name_label = Tk.Label(master, text = 'Last Name:')
+        self.last_name_label = Tk.Label(top, text = 'Last Name:')
         self.last_name_label.grid(row = 1) 
 
         #input for contacts last name
-        self.last_name = Tk.Entry(master)
+        self.last_name = Tk.Entry(top)
         self.last_name.grid(row = 1, column = 1)
 
-        self.address1_label = Tk.Label(master, text = 'Address 1:')
+        self.address1_label = Tk.Label(top, text = 'Address 1:')
         self.address1_label.grid(row = 2)
 
         #input for contacts address1
-        self.address1 = Tk.Entry(master)
+        self.address1 = Tk.Entry(top)
         self.address1.grid(row = 2, column = 1)
 
-        self.adress2_label = Tk.Label(master, text = 'Address 2:')
+        self.adress2_label = Tk.Label(top, text = 'Address 2:')
         self.adress2_label.grid(row = 3)
 
         #input for contacts address2
-        self.address2 = Tk.Entry(master)
+        self.address2 = Tk.Entry(top)
         self.address2.grid(row = 3, column = 1)
 
-        self.email_label = Tk.Label(master, text = 'e-Mail:')
+        self.email_label = Tk.Label(top, text = 'e-Mail:')
         self.email_label.grid(row = 4)
 
         #input for contacts email
-        self.email = Tk.Entry(master)
+        self.email = Tk.Entry(top)
         self.email.grid(row = 4, column = 1)
 
-        self.birthday_label = Tk.Label(master, text = 'Birthday:')
+        self.birthday_label = Tk.Label(top, text = 'Birthday:')
         self.birthday_label.grid(row = 5)
 
         #input for contacts birthday
-        self.birthday = Tk.Entry(master)
+        self.birthday = Tk.Entry(top)
         self.birthday.grid(row = 5, column = 1)
 
-        self.notes_label = Tk.Label(master, text = "Notes")
+        self.notes_label = Tk.Label(top, text = "Notes")
         self.notes_label.grid(row = 6)
 
         #input for notes on contact
-        self.notes = Tk.Entry(master)
+        self.notes = Tk.Entry(top)
         self.notes.grid(row = 6, column = 1)
 
-        self.cancel_button = Tk.Button(master, text = 'Cancel')#NEED TO MAKE BUTTON FUNCTIONAL
+        self.cancel_button = Tk.Button(top, text = 'Cancel')#NEED TO MAKE BUTTON FUNCTIONAL
         self.cancel_button.grid(row = 8, column = 2)
 
-        self.add_button = Tk.Button(master, text= 'Add')#NEED TO MAKE BUTTON FUNCTIONAL
+        self.add_button = Tk.Button(top, text= 'Add')#NEED TO MAKE BUTTON FUNCTIONAL
         self.add_button.grid(row = 7, column = 2)
 
 
