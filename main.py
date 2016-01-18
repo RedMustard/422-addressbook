a445e5854ed7c02bb4803dbb29677bf0b807e7f3
Travis Barnes <ttb@uoregon.edu>
2016-01-18 12:01:45 -0800

    Added functions to main.py

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ Author: Travis Barnes, 18 January 2016
 import AddressBook as ab
 import db
 
+def get_contact():
+	"""Grabs field data and creates an entry object"""
+
+
+def add_contact():
+	"""Adds a contact to the database"""
+
+
+def remove_contact():
+	"""Removes a contact"""
+
+
+def edit_contact():
+	"""Edits a contact"""
+
+
+def new_book():
+	"""Creates a new address book"""
 
 
 if __name__ == "__main__":
