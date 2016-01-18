42877b7b49f4cc4b7534803f668656432f9d7127
Austin Gheen <austingheen@128-223-223-120-wireless.uoregon.edu>
2016-01-18 14:03:18 -0800

    adding AddContact window

--- AddressBook.py
+++ AddressBook.py
@@ -11,13 +11,8 @@ import main as m
 # 		input the name of the database, a call is made to add any existing entries into book_list
 #  Check out item_return to see how the call is made to get_contacts()
 
-
-
-#root = Tk.Tk()
-
 class mainWindow(object):
-    
-    
+
     def item_return(self):
         r = self.item.get()
         m.add_contact(r)
@@ -25,49 +20,93 @@ class mainWindow(object):
         for contact in m.get_contacts('last'):
             self.book_list.insert(Tk.END, contact)
 
+    def popup(self):
+        self.w=AddContactWindow(self.master)
+        self.master.wait_window(self.w)
 
     def __init__(self,master):
         self.master = master
-        master.title("Address Book")
+        master.title('Address Book')
         self.item = Tk.Entry(master)
         self.item.grid(row=2, column = 1, pady = 10)
         self.item.insert(0, 'Enter name')
-        self.submit = Tk.Button(master, text='Submit', command = self.item_return) #NEED TO MAKE BUTTON FUNCTIONAL
+        self.submit = Tk.Button(master, text='Submit', command = self.item_return)
         self.submit.grid(row = 3, column = 1)
         self.scrollbar = Tk.Scrollbar(master)
         self.scrollbar.grid(row = 4, column = 2)
         self.book_list = Tk.Listbox(master, yscrollcommand = self.scrollbar.set)
         self.book_list.grid(row = 4, column = 1, padx = 10, pady = 10, columnspan= 3)
         self.scrollbar.config(command = self.book_list.yview)
+        self.add_button = Tk.Entry(master, text = 'Add', command = self.popup)
+        self.add_button.grid(row= 5, column = 2)
+        #ADD THE ADD BUTTON
 
 
-# return r
+class AddContactWindow():
 
-#scrollbar.config(command = book_list.yview)
+    def __init__():
+        self.master = master
+        master.title('Add Contact')
 
-#root.title("Address Book")
-#root.resizeable(10,10)
+        self.first_name_label = Tk.Label(master, text = 'First Name:')
+        self.first_name_label.grid()
 
-#item = Tk.Entry(root)
-#item.grid(row=2, column = 1, pady = 10)
-#item.insert(0, 'Enter name')
+        #input for contacts first name
+        self.first_name = Tk.Entry(master)
+        self.first_name.grid(row = 0, column = 1)
+
+        self.last_name_label = Tk.Label(master, text = 'Last Name:')
+        self.last_name_label.grid(row = 1) 
+
+        #input for contacts last name
+        self.last_name = Tk.Entry(master)
+        self.last_name.grid(row = 1, column = 1)
+
+        self.address1_label = Tk.Label(master, text = 'Address 1:')
+        self.address1_label.grid(row = 2)
+
+        #input for contacts address1
+        self.address1 = Tk.Entry(master)
+        self.address1.grid(row = 2, column = 1)
+
+        self.adress2_label = Tk.Label(master, text = 'Address 2:')
+        self.adress2_label.grid(row = 3)
+
+        #input for contacts address2
+        self.address2 = Tk.Entry(master)
+        self.address2.grid(row = 3, column = 1)
+
+        self.email_label = Tk.Label(master, text = 'e-Mail:')
+        self.email_label.grid(row = 4)
+
+        #input for contacts email
+        self.email = Tk.Entry(master)
+        self.email.grid(row = 4, column = 1)
+
+        self.birthday_label = Tk.Label(master, text = 'Birthday:')
+        self.birthday_label.grid(row = 5)
+
+        #input for contacts birthday
+        self.birthday = Tk.Entry(master)
+        self.birthday.grid(row = 5, column = 1)
+
+        self.notes_label = Tk.Label(master, text = "Notes")
+        self.notes_label.grid(row = 6)
+
+        #input for notes on contact
+        self.notes = Tk.Entry(master)
+        self.notes.grid(row = 6, column = 1)
+
+        self.cancel_button = Tk.Button(master, text = 'Cancel')#NEED TO MAKE BUTTON FUNCTIONAL
+        self.cancel_button.grid(row = 8, column = 2)
+
+        self.add_button = Tk.Button(master, text= 'Add')#NEED TO MAKE BUTTON FUNCTIONAL
+        self.add_button.grid(row = 7, column = 2)
 
 
-#sumbit button to make a Address book names after the name entered
-#submit = Tk.Button(root, text='Submit', command = item_return) #NEED TO MAKE BUTTON FUNCTIONAL
-#submit.grid(row = 3, column = 1)
-
-#scroll bar for listbox of address books
-#scrollbar = Tk.Scrollbar(root)
-#scrollbar.grid(row = 4, column = 2)
-
-#list of all the address books
-#book_list = Tk.Listbox(root, yscrollcommand = scrollbar.set)
-#book_list.grid(row = 4, column = 1, padx = 10, pady = 10, columnspan= 3)
-
-#scrollbar.config(command = book_list.yview)
 
 if __name__ == "__main__":
-    root = Tk.Tk()
-    m=mainWindow(root)
-    root.mainloop()
+    master = Tk.Tk()
+    m=mainWindow(master)
+    w=AddContactWindow(master)
+    master.mainloop()
